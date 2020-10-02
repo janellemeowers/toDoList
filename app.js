42003a222349a7781e33f47bec336b2ca19913f3
janelle myers <janellemyers@janelleacintosh.verizon.net>
2020-10-02 16:39:19 -0400

    refractor

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+//module for date
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-  var items = [];
+  const items = [];
 
 app.set('view engine', 'ejs');
 
@@ -13,22 +15,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public")); //tell express where your static files are
 
 app.get("/", function(req, res){
-  var today = new Date();
-  var options = {
-    weekday: "long",
-    day: "numeric",
-   month: "long"
- };
 
- var day=today.toLocaleDateString("en-US", options);
-
+const day = date.getDate();
     res.render("list", {kindOfDay: day, newListItems: items} ); //looks inside views folder so it is a must, also must have ejs extension, passes through a variable
 
 });
 
 //post request, new data sent to home route
 app.post("/", function(req, res){
-var item = req.body.newItem;
+const item = req.body.newItem;
   items.push(item);
 
   res.redirect("/"); //home route
---
+++ date.js
@@ -0,0 +1,15 @@
+//jshint esversion:6
+
+//module exports
+
+exports.getDate = function() {
+
+const today = new Date();
+const options = {
+  weekday: "long",
+  day: "numeric",
+ month: "long"
+};
+
+return today.toLocaleDateString("en-US", options);
+};
